4acaa8d2b48e4c6153cc5a6f20723c23c0c5b431
Flor Tello <flor.26071994@gmail.com>
2017-02-23 17:33:06 -0500

    Agregando boton limpiar

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,36 +1,18 @@
-var enviar = document.getElementById("mostrarDatos");
+
 var result = document.getElementById("result");
 
-enviar.onclick = function(){
+function mostrarDatos(){
  	var nombre = document.getElementById("textNombre").value;
  	var apellido = document.getElementById("textApellidos").value;
  	var dni = document.getElementById("textDNI").value;
  	var direccion = document.getElementById("textDireccion").value;
  	
- 	if(nombre != '' && apellido != "" && dni != ""  && direccion != ""){
  		var mostrar = "<ul><li>Nombre: "+ nombre + "</li>"+
 	 				  "<li>Apellidos: "+ apellido +"</li>"+
 	 				  "<li>DNI: "+ dni + "</li>"+
 	 				  "<li>Dirección: "+ direccion + "</li></ul>";
  		result.innerHTML=mostrar;
- 	}
-
+ 	
  }
-
-/* function mostrarDatos(){
- 	var nombre = document.getElementById("textNombre").value;
- 	var apellido = document.getElementById("textApellidos").value;
- 	var dni = document.getElementById("textDNI").value;
- 	var direccion = document.getElementById("textDireccion").value;
- 	
- 	if(nombre != '' && apellido != "" && dni != ""  && direccion != ""){
- 		var mostrar = "<ul><li>Nombre: "+ nombre + "</li>"+
-	 				  "<li>Apellidos: "+ apellido +"</li>"+
-	 				  "<li>DNI: "+ dni + "</li>"+
-	 				  "<li>Dirección: "+ direccion + "</li></ul>";
- 		result.innerHTML=mostrar;
- 	}
-
- }*/
 
 
--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,23 +1,7 @@
-var enviar = document.getElementById("mostrarDatos");
+//var enviar = document.getElementById("mostrarDatos");
 var result = document.getElementById("result");
 
-enviar.onclick = function(){
- 	var nombre = document.getElementById("textNombre").value;
- 	var apellido = document.getElementById("textApellidos").value;
- 	var dni = document.getElementById("textDNI").value;
- 	var direccion = document.getElementById("textDireccion").value;
- 	
- 	if(nombre != '' && apellido != "" && dni != ""  && direccion != ""){
- 		var mostrar = "<ul><li>Nombre: "+ nombre + "</li>"+
-	 				  "<li>Apellidos: "+ apellido +"</li>"+
-	 				  "<li>DNI: "+ dni + "</li>"+
-	 				  "<li>Dirección: "+ direccion + "</li></ul>";
- 		result.innerHTML=mostrar;
- 	}
-
- }
-
-/* function mostrarDatos(){
+/*enviar.onclick = function(){
  	var nombre = document.getElementById("textNombre").value;
  	var apellido = document.getElementById("textApellidos").value;
  	var dni = document.getElementById("textDNI").value;
@@ -32,5 +16,21 @@ enviar.onclick = function(){
  	}
 
  }*/
+
+function mostrarDatos(){
+ 	var nombre = document.getElementById("textNombre").value;
+ 	var apellido = document.getElementById("textApellidos").value;
+ 	var dni = document.getElementById("textDNI").value;
+ 	var direccion = document.getElementById("textDireccion").value;
+ 	
+ 	if(nombre != '' && apellido != "" && dni != ""  && direccion != ""){
+ 		var mostrar = "<ul><li>Nombre: "+ nombre + "</li>"+
+	 				  "<li>Apellidos: "+ apellido +"</li>"+
+	 				  "<li>DNI: "+ dni + "</li>"+
+	 				  "<li>Dirección: "+ direccion + "</li></ul>";
+ 		result.innerHTML=mostrar;
+ 	}
+
+ }
 
 
